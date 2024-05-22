9055b96ec3faeed6e684df026e3f91bde6d9d0a7
umairghani156 <umairghani156@gmail.com>
2024-05-22 20:46:04 +0500

    clientApp

---
+++ src/pages/login/Login.jsx
@@ -0,0 +1,14 @@
+import "./login.css";
+
+const Login = ()=>{
+    return(
+        <form>
+            <input type="text" name="" id="" />
+            <input type="text" name="" id="" />
+            <input type="text" name="" id="" />
+            <button>SignUp</button>
+        </form>
+    )
+}
+
+export default Login;
---
+++ src/pages/signup/Signup.jsx
@@ -0,0 +1,13 @@
+import "./signup.css";
+
+const Signup = ()=>{
+    return(
+        <form>
+            <input type="text" name="" id="" />
+            <input type="text" name="" id="" />
+            <input type="text" name="" id="" />
+            <button>SignUp</button>
+        </form>
+    )
+}
+export default Signup;
